dcb3ac4092ef80eeb7dfd05b925e0634099411b2
kim <chfaos120@naver.com>
2018-09-04 18:11:05 +0900

    my

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,73 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-      
-        Scanner sc =new Scanner(System.in);
+    //asdfasdfasfsdafasfadsfdasfadsfasfasdfsadfsad
+    static class vari
+    {
+        protected String sori;
+        protected String dom2;
+        protected int dobi;
+        
+        public String getSori() {
+            return sori;
+        }
+        public void setSori(String sori) {
+            this.sori = sori;
+        }
+        public String getDom2() {
+            return dom2;
+        }
+        public void setDom2(String dom2) {
+            this.dom2 = dom2;
+        }
+        public int getDobi() {
+            return dobi;
+        }
+        public void setDobi(int dobi) {
+            this.dobi = dobi;
+        }
+        
+    }
+    static vari[] va=new vari[100];
+    static int index=0;
+    static Scanner sc =new Scanner(System.in);
+    
+    static void output()
+    {
+        for(int i=0;i<index; i++)
+        {
+        System.out.printf("%s , %s , %d",va[i].getSori(),va[i].getDom2(),va[i].getDobi());
+        }
+    }
+    
+    
+    static void input()
+    {
+       
+        while(true)
+        {
+        vari v=new vari();
         
         System.out.print("쏴리질러!!!");
-        String sori=sc.nextLine();
+        v.setSori(sc.nextLine());
         System.out.print("둠칫둠칫");
-        String dom2=sc.nextLine();
+        v.setDom2(sc.nextLine());
         System.out.print("두비두밥");
-        int dobi=sc.nextInt();
+        v.setDobi(sc.nextInt());
+        System.out.print("게속 둠칫 ? y/n ");
+        String yn=sc.nextLine();
         
-        System.out.printf("%s , %s , %d",sori ,dom2 , dobi);
+        va[index++] =v;
+        
+        if(yn.toLowerCase().equals("n"))
+                break;
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        input();
+        output();
+        sc.close();
     }
 }
